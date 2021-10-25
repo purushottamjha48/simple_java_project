4e53ad6ba06ec2daacea8f16bc3c13c16710b64a
purushottamjha48 <41940616+purushottamjha48@users.noreply.github.com>
2021-10-25 14:59:26 +0530

    Update ExampleOddEven.java

--- ExampleOddEven.java
+++ ExampleOddEven.java
@@ -5,11 +5,11 @@ public static void main(String[] args)
 int num=11;
 if(num%2==0)
 {
-System.out.println("Even");
+System.out.println("Even Number");
 }
 else
 {
-System.out.println("Odd");
+System.out.println("Odd Number");
 }
 }
 }
